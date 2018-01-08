736ea9893920e461d0e5d9d3c82ac94bb34bcec9
yangmeeso <yangmeeso@gmail.com>
2018-01-08 02:10:16 -0800

    Added more functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,16 +9,11 @@ var connection = mysql.createConnection ({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    allItems();
-});
-
 function allItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         // console.log(res);
+        console.log("** All items listed on Bamazon ** ")
         for (var i = 0; i < res.length; i++) {
             console.log(`\nID: ${res[i].item_id}
             \nProduct Name: ${res[i].product_name}
@@ -29,4 +24,47 @@ function allItems() {
         }
         connection.end();
     })
+};
+
+function buyProducts (answerId, answerUnit) {
+    console.log(`Product ID you entered: ${answerId}
+    \nOrder quantity you entered: ${answerUnit}`);
 }
+
+var questions = [
+    {
+        type: "input",
+        message: "Please enter the product ID that you would like to buy.",
+        name: "id",
+        validate: function idVal(id) {
+            return id !== "";
+        },
+        filter: Number
+
+    },{
+        type: "input",
+        message: "How many units of the product they would like to buy?",
+        name: "unit",
+        validate: function unitVal(unit) {
+            return id !== "";            
+        },
+        filter: Number  
+    }];
+
+inquirer.prompt(questions, answerId, answerUnit);
+    .then(function(inquirerResponse) {
+        if(inquirerResponse.input <= 10) {
+            console.log(inquirerResponse.id);
+            console.log(inquirereResponse.unit);
+        }
+        else {
+            console.log("Invalid Input.");
+        }
+    });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    allItems();
+});
+
